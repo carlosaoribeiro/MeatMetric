2f9eda518bef7d561b1e2234fd1e84b06150f8d3
carlosribeiro <carlos.a.o.r@icloud.com>
2025-04-03 08:06:52 -0500

    remove warnings gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,10 +48,10 @@ dependencies {
     annotationProcessor(libs.lifecycle.compiler)
 
     // ROOM - apenas essa parte, sem duplicar
-    implementation("androidx.room:room-runtime:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
 
-    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation (libs.cardview)
 
 }
 
